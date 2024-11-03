318c0a3a9adc2c08638f3b7b44cc86d33e63e0a8
Tomáš Dubský <td.zadne@seznam.cz>
2024-11-03 13:29:54 +0100

    Rename aligned types

--- cmake/RealProject/static_code_gen/GenerateStd140AlignedTypes.py
+++ cmake/RealProject/static_code_gen/GenerateStd140AlignedTypes.py
@@ -2,7 +2,7 @@
 
 prefixes = ["b", "i8", "u8", "i16", "u16", "i32", "u32", "i64", "u64", "u16", "", "d"]
 
-print("// All vecs aligned to 16 bytes (for std 140 arrays)")
+print("// All vecs aligned to 16 bytes (for std140 arrays)")
 for prefix in prefixes:
     for dims in range(1, 5):
-        print(f"struct alignas(16) {prefix}vec{dims}_a16 : public glm::{prefix}vec{dims} {{}};")
+        print(f"struct alignas(16) {prefix}vec{dims}_140 : public glm::{prefix}vec{dims} {{}};")
